193aff4c61a467b08ae18ce4dace8c97c503a990
Niles Tanner <niles.tanner@gmail.com>
2018-02-28 23:03:09 -0600

    added line

---
+++ common.js
@@ -0,0 +1,12 @@
+const distance = (point1, point2) => {
+	return Math.sqrt( Math.pow(point1.x-point2.x, 2) + Math.pow(point1.y-point2.y, 2) );
+};
+
+const isVertical = (points) => {
+	return Math.abs(points[0].x - points[points.length - 1].x) <  Math.abs(points[0].y - points[points.length - 1].y);
+}
+
+module.exports = {
+  distance,
+  isVertical
+};
