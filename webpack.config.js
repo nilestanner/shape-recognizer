07369bc259c637ecdface8e6c661451c0132b474
Niles Tanner <niles.tanner@gmail.com>
2018-03-09 20:12:03 -0600

    changed dist file names

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'shape-recognizer.min.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  mode:'production'
 };
